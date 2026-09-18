273e34b87fcb31abccc9fdf8e97e04e6c5052a9c
harshithnaredla <naredlaharshith27@gmail.com>
2026-09-18 10:44:01 +0530

    Time: 23 ms (15.77%) | Memory: 19.5 MB (30.24%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nextGreaterElement(self, nums1,nums2):
+        answer=[]
+        for num in nums1:
+            index=nums2.index(num)
+            greater=-1
+            for i in range(index + 1,len(nums2)):
+                if nums2[i]>num:
+                    greater =nums2[i]
+                    break
+            answer.append(greater)
+        return answer
